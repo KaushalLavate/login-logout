9e65e49e4356af349ef72920116e6cb0c65bc68a
KaushalLavate <kaushallavate@gmail.com>
2020-03-07 11:38:52 +0530

    Updated Static Page

--- calc/views.py
+++ calc/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'base.html',{'name':'Kaushal'});
+
+def add(request):
+    sum1=0
+    val1=int(request.POST["num1"])
+    val2=int(request.POST["num2"])
+    sum1=val1+val2
+    print(sum1)
+    return render(request,'base.html',{'result':sum1});
